86263409cdd38d1c8d4df87d2229cb4b6ce2a417
yash3199 <69784709+yash3199@users.noreply.github.com>
2020-09-08 11:51:17 +0530

    add initial drumkit files

---
+++ index.js
@@ -0,0 +1,48 @@
+/*var size = document.querySelectorAll(".drum").length;*/
+for(var i=0;i<6;i++){
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+var buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
+buttonAnimation(buttonInnerHTML);
+});
+}
+document.addEventListener("keydown", function(event){
+makeSound(event.key);
+buttonAnimation(event.key);
+});
+function makeSound(key){
+    switch(key){
+        case "z":
+            var tom1= new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a1":
+            var tom2= new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "v":
+            var tom3= new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "i":
+            var tom4= new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "a2":
+            var snare= new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "n":
+            var crash= new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+    }
+}
+function buttonAnimation(currentkey){
+    var activeButton= document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed")
+    },100);
+
+}
